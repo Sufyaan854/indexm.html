c138e15b31cb8467553f38da098a5457d3dd983a
Sufyaan854 <bilalsufyaan854@gmail.com>
2024-03-02 10:46:56 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// You can add JavaScript functionality here if needed
+
+// Example: alert when login button is clicked
+document.getElementById('loginBtn').addEventListener('click', function() {
+    alert('Login button clicked!');
+});
+
+// Example: alert when register button is clicked
+document.getElementById('registerBtn').addEventListener('click', function() {
+    alert('Register button clicked!');
+});
